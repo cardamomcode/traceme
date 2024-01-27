30ae8716ea9c4e01a36f782ce471894da77a3697
Dag Brattli <dag@brattli.net>
2024-01-27 21:37:05 +0100

    Add CI actions (#1)

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Traceme tests."""
--- traceme/__init__.py
+++ traceme/__init__.py
@@ -1,1 +1,6 @@
-from traceme import TraceMe, trace
+"""Traceme package."""
+from ._version import __version__
+from .traceme import TraceMe, trace
+
+
+__all__ = ["TraceMe", "trace", "__version__"]
